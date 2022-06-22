b775a393ecac86c66a06495eff3d765a782815b8
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-06-22 20:07:45 +0530

    Add files via upload

---
+++ lab6/index1.php
@@ -0,0 +1,24 @@
+<?php
+$handle = fopen("visitors.txt", "r") or die("Unable to open file");
+$count = fread($handle, 16);
+fclose($handle);
+$handle = fopen("visitors.txt", "w") or die("Unable to open file");
+fwrite($handle, ++$count);
+fclose($handle);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>6 Visitor count</title>
+<style>
+h1, h2 {
+text-align: center;
+}
+</style>
+</head>
+<body>
+<h1>Welcome To The Website</h1>
+<h2>Total Visitors : <?php echo $count ?></h2>
+</body>
+</html>
